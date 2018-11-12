db4622373314ebcbfc03a04cd80a5d4a9e1ed804
neec <neec@localhost.localdomain>
2018-11-12 15:01:05 +0900

    add index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Login</title>
+	</head>
+	<body>
+		<h1>秘密のチャット</h1>
+		<form action="chat.php">
+			<input type="text" name="uname">
+			<button>ログイン</button>
+		</form>
+	</body>
+</html>
