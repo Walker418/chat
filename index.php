e39d0d2d1c5ba0be08a6769e53ade71fc2764e73
Walker418 <walker418@hotmail.com>
2019-01-03 20:45:55 +0900

    Layout changed

--- index.php
+++ index.php
@@ -2,14 +2,50 @@
 <html>
 	<head>
 		<title>ログイン</title>
+		<style>
+			.loginform
+			{
+				border: 5px solid #5abbe4;
+				padding: 25px;
+				width: 300px;
+			}
+			h1
+			{
+				font-size: 24px;
+			}
+			.textfield_label
+			{
+				display: inline-block;
+				width: 8em;
+			}
+			.loginbutton
+			{
+				font-size: 16px;
+			}
+			.registration
+			{
+				text-align: right;
+			}
+		</style>
 	</head>
 	<body>
-		<h1>ログイン</h1>
-		<form action="chat.php" method="POST">
-			ユーザーID:<input type="text" name="id" value="<?= $_COOKIE['id'] ?>"><br>
-			パスワード:<input type="password" name="pw"><br>
-			<button>ログイン</button>
-		</form>
-		<a href="userinfo.php">ユーザー登録</a>
+		<div class="loginform">
+			<h1>ログイン</h1>
+			<form action="chat.php" method="POST">
+				<p>
+					<label for="userid" class="textfield_label">ユーザーID:</label>
+					<input type="text" name="id" value="<?= $_COOKIE['id'] ?>" autofocus required>
+				</p>
+				<p>
+					<label for="password" class="textfield_label">パスワード:</label>
+					<input type="password" name="pw" required>
+				</p>
+				<button class="loginbutton">ログイン</button>
+			</form>
+			</br>
+			<p class="registration">
+				<a href="userinfo.php" >ユーザー登録はこちら</a>
+			</p>
+		</div>
 	</body>
 </html>
--- userinfo.php
+++ userinfo.php
@@ -2,15 +2,53 @@
 <html>
 	<head>
 		<title>ユーザー登録</title>
+		<style>
+			.userinfoform
+			{
+				border: 5px solid #5abbe4;
+				padding: 25px;
+				width: 335px;
+			}
+			h1
+			{
+				font-size: 24px;
+			}
+			.textfield_label
+			{
+				display: inline-block;
+				width: 10em;
+			}
+			.submitbutton
+			{
+				font-size: 16px;
+			}
+			.return
+			{
+				text-align: right;
+			}
+		</style>
 	</head>
 	<body>
-		<h1>ユーザー登録</h1>
-		<form action="useradded.php" method="POST">
-			ユーザー名：<input type="text" name="uname"><br>
-			パスワード：<input type="password" name="pw"><br>
-			パスワードの再入力：<input type="password" name="pw_conf"><br>
-			<button>ユーザー情報を登録</button>
-		</form>
-		<a href="index.php">ログイン画面に戻る</a>
+		<div class="userinfoform">
+			<h1>ユーザー登録</h1>
+			<form action="useradded.php" method="POST">
+				<p>
+					<label for="username" class="textfield_label">ユーザー名：</label>
+					<input type="text" name="uname" autofocus required>
+				</p>
+				<p>
+					<label for="password" class="textfield_label">パスワード：</label>
+					<input type="password" name="pw" required>
+				</p>
+				<p>
+					<label for="password_conf" class="textfield_label">パスワードの再入力：</label>
+					<input type="password" name="pw_conf" required>
+				</p>
+					<button  class="submitbutton">ユーザー情報を登録</button>
+			</form>
+			<p class="return">
+				<a href="index.php">ログイン画面に戻る</a>
+			</p>
+		</div>
 	</body>
 </html>
